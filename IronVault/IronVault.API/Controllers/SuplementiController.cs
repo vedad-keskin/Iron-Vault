ec5e1bf23c5a8be72799b1ffe40902712d3798ff
Vedad <vedad.keskin98@gmail.com>
2024-03-10 23:29:46 +0100

    Dodavanje API-ja aplikacije

---
+++ IronVault/IronVault.API/Controllers/SuplementiController.cs
@@ -0,0 +1,26 @@
+﻿using IronVault.Model;
+using IronVault.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace IronVault.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SuplementiController : ControllerBase
+    {
+        protected ISuplementiService _service;
+
+        public SuplementiController(ISuplementiService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public List<Suplementi> GetList()
+        {
+            return _service.GetList(); 
+        }
+
+    }
+}
