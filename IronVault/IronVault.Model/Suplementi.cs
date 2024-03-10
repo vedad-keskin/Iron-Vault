7043be5c008276c3e60321716ceef9d21ad62f68
Vedad <vedad.keskin98@gmail.com>
2024-03-10 23:30:01 +0100

    Dodavanje projekta za modele

---
+++ IronVault/IronVault.Model/Suplementi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronVault.Model
+{
+    public class Suplementi
+    {
+        public int SuplementId { get; set; }
+        public string Naziv { get; set; }
+        public float Cijena { get; set; }
+        //public float Gramaza { get; set; }
+        //public string Opis { get; set; }
+        //public string? Slika { get; set; }
+
+        //[ForeignKey(nameof(Dobavljac))]
+        //public int DobavljacID { get; set; }
+        //public Dobavljac Dobavljac { get; set; }
+        //[ForeignKey(nameof(Kategorija))]
+        //public int KategorijaID { get; set; }
+        //public Kategorija Kategorija { get; set; }
+    }
+}
