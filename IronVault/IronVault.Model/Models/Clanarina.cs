7d564b3e40fc9b589bdcf0b76d48906cae80e961
Vedad Keskin <vedad.keskin98@gmail.com>
2024-08-05 23:42:21 +0200

    Implementacija clanarina na backendu

---
+++ IronVault/IronVault.Model/Models/Clanarina.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronVault.Model.Models
+{
+    public partial class Clanarina
+    {
+        public int ClanarinaId { get; set; }
+
+        public string Naziv { get; set; } = null!;
+
+        public float Cijena { get; set; }
+
+        public string Opis { get; set; } = null!;
+    }
+}
---
+++ IronVault/IronVault.Model/SearchObjects/ClanarinaSearchObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronVault.Model.SearchObjects
+{
+    public class ClanarinaSearchObject : BaseSearchObject
+    {
+        public string? Naziv { get; set; }
+
+    }
+}
