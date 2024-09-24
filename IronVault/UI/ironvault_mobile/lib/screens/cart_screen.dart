65aa448801aee3abdf83b75d667b72bed24b4aea
Vedad Keskin <vedad.keskin98@gmail.com>
2024-09-24 02:33:27 +0200

    Ukupno counter

--- IronVault/UI/ironvault_mobile/lib/screens/cart_screen.dart
+++ IronVault/UI/ironvault_mobile/lib/screens/cart_screen.dart
@@ -10,7 +10,7 @@ class CartScreen extends StatefulWidget {
   static const String routeName = "/cart";
   final int id; // Add this
 
-  const CartScreen(this.id,{Key? key}) : super(key: key);
+  const CartScreen(this.id, {Key? key}) : super(key: key);
 
   @override
   State<CartScreen> createState() => _CartScreenState();
@@ -22,134 +22,152 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
   @override
   void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
     super.didChangeDependencies();
     _cartProvider = context.watch<CartProvider>();
     _orderProvider = context.read<OrderProvider>();
   }
 
-@override
-Widget build(BuildContext context) {
-  return MasterScreen(
-    "Korpa",
-    index: 2,
-    id: widget.id, // Set the desired index here
-    child: Column(
-      children: [
-        Expanded(child: _buildSuplementCardList()),
-        _buildBuyButton()
-      ],
-    ),
-  );
-}
+  double _calculateTotalPrice() {
+    double totalPrice = 0.0;
+    for (var item in _cartProvider.cart.items) {
+      totalPrice += item.suplement.cijena! * item.count;
+    }
+    return totalPrice;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MasterScreen(
+      "Korpa",
+      index: 2,
+      id: widget.id, // Set the desired index here
+      child: Column(
+        children: [
+          Expanded(child: _buildSuplementCardList()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: _buildTotalPriceLabel(),
+          ),
+          _buildBuyButton(),
+        ],
+      ),
+    );
+  }
 
   Widget _buildSuplementCardList() {
     return Container(
       child: ListView.builder(
         itemCount: _cartProvider.cart.items.length,
         itemBuilder: (context, index) {
-          print("rendering element");
           return _buildSuplementCard(_cartProvider.cart.items[index]);
         },
       ),
     );
   }
-  
-Widget _buildSuplementCard(CartItem item) {
-  return Container(
-// Add some margin for spacing
-    decoration: BoxDecoration(
-      border: Border.all(color: Colors.grey), // Set border color and width
- // Optional: Rounded corners
-    ),
-    child: Stack(
-      children: [
-        // The main content of the card
-        ListTile(
-          contentPadding: const EdgeInsets.all(12),
-          leading: item.suplement.slika == null 
-              ? const Placeholder() 
-              : imageFromString(item.suplement.slika!),
-          title: Text(item.suplement.naziv ?? "",
-          style: const TextStyle(
-              
-              fontWeight: FontWeight.bold,
-            ),),
-          subtitle: Text(item.suplement.cijena.toString()),
-          trailing: Text(
-            item.count.toString(),
-            style: const TextStyle(
-              fontSize: 25, // Make the count text bigger
-              fontWeight: FontWeight.bold,
+
+  Widget _buildSuplementCard(CartItem item) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey), // Set border color and width
+      ),
+      child: Stack(
+        children: [
+          ListTile(
+            contentPadding: const EdgeInsets.all(12),
+            leading: item.suplement.slika == null
+                ? const Placeholder()
+                : imageFromString(item.suplement.slika!),
+            title: Text(
+              item.suplement.naziv ?? "",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text("${item.suplement.cijena} KM"),
+            trailing: Text(
+              item.count.toString(),
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: IconButton(
+              icon: const Icon(Icons.close, color: Colors.red),
+              onPressed: () {
+                setState(() {
+                  _cartProvider.removeFromCart(item.suplement);
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+Widget _buildTotalPriceLabel() {
+  double totalPrice = _calculateTotalPrice();
+  return Padding(
+    padding: const EdgeInsets.only(right: 10.0), // Add right padding
+    child: Align(
+      alignment: Alignment.centerRight, // Align to the right
+      child: Text(
+        "Ukupno za uplatiti ${totalPrice.toStringAsFixed(2)} KM",
+        textAlign: TextAlign.end, // Ensure text is aligned to the right
+        style: const TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+          color: Colors.grey, // Set text color to gray
         ),
-        // The 'X' button in the top-right corner
-        Positioned(
-          right: 0,
-          top: 0,
-          child: IconButton(
-            icon: const Icon(Icons.close, color: Colors.red),
-            onPressed: () {
-              setState(() {
-                _cartProvider.removeFromCart(item.suplement); // Remove item from cart
-              });
-            },
+      ),
+    ),
+  );
+}
+
+  Widget _buildBuyButton() {
+    return SizedBox(
+      width: double.infinity, // Full width button
+      child: TextButton(
+        onPressed: () {
+          List<Map> items = [];
+          _cartProvider.cart.items.forEach((item) {
+            items.add({
+              "suplementId": item.suplement.suplementId,
+              "kolicina": item.count
+            });
+          });
+
+          var order = {
+            "items": items
+          };
+
+          //_orderProvider.insert(order)
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.green, // Button background color
+          foregroundColor: Colors.white, // Text color
+          padding: const EdgeInsets.symmetric(vertical: 14), // Padding for height
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero, // No rounded corners, squared
           ),
         ),
-      ],
-    ),
-  );
-}
-
-
-  
-Widget _buildBuyButton() {
-  return SizedBox(
-    width: double.infinity, // Full width button
-    child: TextButton(
-      onPressed: () {
-        List<Map> items = [];
-        _cartProvider.cart.items.forEach((item) {
-          items.add({
-            "suplementId": item.suplement.suplementId,
-            "kolicina": item.count
-          });
-        });
-
-        var order = {
-          "items": items
-        };
-
-        //_orderProvider.insert(order)
-      },
-      style: TextButton.styleFrom(
-        backgroundColor: Colors.green, // Button background color
-        foregroundColor: Colors.white, // Text color
-        padding: const EdgeInsets.symmetric(vertical: 14), // Padding for height
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.zero, // No rounded corners, squared
+        child: const Text(
+          "Kupi",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      child: const Text(
-        "Kupi",
-        style: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-  );
-}
-
-
-
-
- 
+    );
+  }
 }
